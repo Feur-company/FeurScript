8e3c94bca139ffeaad1a8302ac25e16e08a28b0b
JdM <julien@rmbi.ch>
2023-05-17 13:25:38 +0200

    split line interpreter

---
+++ interpreter/main.py
@@ -0,0 +1,16 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Run FeurScript program.')
+    parser.add_argument('-f','--file', help='Input FeurScript file', required=True)
+    args = parser.parse_args()
+    filename = args.file
+    file = open(filename, 'r')
+    code = file.read()
+    code = code.replace(" ", "")
+    code = code.splitlines()
+    for line in code:
+        print(line)
+
+if __name__ == '__main__':
+    main()
